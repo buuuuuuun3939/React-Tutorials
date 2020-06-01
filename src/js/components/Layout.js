fa18f3853603f4f7bda66e0d89a7f4248863d753
buuuuuuun3939 <buuuun3939@gmail.com>
2020-06-02 06:31:20 +0900

    initial commit

---
+++ src/js/components/Layout.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Header from "./Header"
+import Footer from "./Footer"
+
+export default class Layout extends React.Component {
+    constructor() {
+        super();
+        this.state = {name: "hogemaru"}
+    }
+    render() {
+        setTimeout(
+            () => { this.setState({ name: "Hello" }); }, 1000);
+        let components = [<Header />, <Footer />]
+        return (
+            <div>
+                {this.state.name}
+                {components}
+            </div>
+        );
+    }
+}
